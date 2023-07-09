e7b10f9adc54906c69c078a52558f0684a57613b
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2023-07-09 14:50:19 +0900

    update .NET6に更新。

--- DotNetCoreMockWebServer/Startup.cs
+++ DotNetCoreMockWebServer/Startup.cs
@@ -14,8 +14,6 @@ namespace DotNetCoreMockWebServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<KestrelServerOptions>(options =>
@@ -29,7 +27,6 @@ namespace DotNetCoreMockWebServer
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
